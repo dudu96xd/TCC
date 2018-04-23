37293d59f7d425ad83192cefb9e667e8704cef00
Igor Eduardo Troncoso Salvio <igor.troncoso.salvio@accenture.com>
2018-04-23 14:13:11 -0300

    adicao de telas de login, criacao de contas, adicao de dependencias de estilo e activities correspondentes

---
+++ app/src/main/java/grupo9/tcc2018/usjt/com/tcc2018/FacebookActivity.java
@@ -0,0 +1,7 @@
+package grupo9.tcc2018.usjt.com.tcc2018;
+
+import android.support.v4.app.FragmentActivity;
+
+public class FacebookActivity extends FragmentActivity {
+    //TODO: IMPLEMENTAR ESSA BAGAÇA
+}
--- app/src/main/java/grupo9/tcc2018/usjt/com/tcc2018/MapActivity.java
+++ app/src/main/java/grupo9/tcc2018/usjt/com/tcc2018/MapActivity.java
@@ -2,14 +2,16 @@ package grupo9.tcc2018.usjt.com.tcc2018;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,11 +19,43 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    Location myLocation;
+    boolean isGPSEnabled,isNetworkEnabled,canGetLocation;
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    double latitude,longitude;
+
+    LocationListener locationListenerGPS=new LocationListener() {
+        @Override
+        public void onLocationChanged(android.location.Location location) {
+            latitude=location.getLatitude();
+            longitude=location.getLongitude();
+            //String msg="New Latitude: "+latitude + "New Longitude: "+longitude;
+            //Toast.makeText(mContext,msg,Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +66,107 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
+    public android.location.Location getLocation() {
+        try {
+            if(checkLocationPermission()){
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                // getting GPS status
+                isGPSEnabled = locationManager
+                        .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+                // getting network status
+                if (myLocation == null) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerGPS);
+                    Log.d("GPS Enabled", "GPS Enabled");
+                    if (locationManager != null) {
+                        myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if (myLocation != null) {
+                            isNetworkEnabled = true;
+                            latitude = myLocation.getLatitude();
+                            longitude = myLocation.getLongitude();
+                        }
+                    }
+                }
+
+                if (!isGPSEnabled && !isNetworkEnabled) {
+                    // no network provider is enabled
+                } else {
+                    this.canGetLocation = true;
+                    if (isNetworkEnabled) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.NETWORK_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerGPS);
+                        Log.d("Network", "Network Enabled");
+                        if (locationManager != null) {
+                            myLocation = locationManager
+                                    .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                            LatLng myLatLng = new LatLng(latitude,
+                                    longitude);
+
+                            CameraPosition myPosition = new CameraPosition.Builder()
+                                    .target(myLatLng).build();
+                            mMap.animateCamera(
+                                    CameraUpdateFactory.newCameraPosition(myPosition));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLatLng));
+                            mMap.animateCamera(CameraUpdateFactory.zoomTo(14), 2000, null);
+                            mMap.clear();
+                            mMap.addMarker(new MarkerOptions().position(myLatLng).title("Localização atual."));
+                            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                        }
+                    }
+                    // if GPS Enabled get lat/long using GPS Services
+                    if (isGPSEnabled) {
+                        if (myLocation == null) {
+                            locationManager.requestLocationUpdates(
+                                    LocationManager.GPS_PROVIDER,
+                                    MIN_TIME_BW_UPDATES,
+                                    MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerGPS);
+                            Log.d("GPS", "GPS Enabled");
+                            if (locationManager != null) {
+                                myLocation = locationManager
+                                        .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return myLocation;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+
+            case 0: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission granted", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
+    public boolean checkLocationPermission() {
+        String permission = "android.permission.ACCESS_FINE_LOCATION";
+        int res = this.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+
 
 
     /**
@@ -46,47 +181,43 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         }
+        if(!checkLocationPermission()){
+            // Add a marker in São Paulo and move the camera
+            LatLng sp = new LatLng(-23.533773, -46.625290);
+            mMap.addMarker(new MarkerOptions().position(sp).title("Marker in São Paulo"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sp));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(14), 2000, null);
+        }
+        else
+            myLocation = this.getLocation();
 
-        if (checkLocationPermission()) {
+        mMap.setTrafficEnabled(true);
+        mMap.setBuildingsEnabled(false);
 
-            LocationManager locationManager = (LocationManager)
-                    getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
+    }
 
-            Location myLocation = locationManager.getLastKnownLocation(locationManager
-                    .getBestProvider(criteria, false));
-            double latitude = myLocation.getLatitude();
-            double longitude = myLocation.getLongitude();
+    private LatLngBounds adjustBoundsForMaxZoomLevel(LatLngBounds bounds) {
+        LatLng sw = bounds.southwest;
+        LatLng ne = bounds.northeast;
+        double deltaLat = Math.abs(sw.latitude - ne.latitude);
+        double deltaLon = Math.abs(sw.longitude - ne.longitude);
 
-            LatLng myLatLng = new LatLng(myLocation.getLatitude(),
-                    myLocation.getLongitude());
-
-            CameraPosition myPosition = new CameraPosition.Builder()
-                    .target(myLatLng).build();
-            mMap.animateCamera(
-                    CameraUpdateFactory.newCameraPosition(myPosition));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLatLng));
-            mMap.clear();
-            mMap.addMarker(new MarkerOptions().position(myLatLng).title("Localização atual."));
-
+        final double zoomN = 0.005; // minimum zoom coefficient
+        if (deltaLat < zoomN) {
+            sw = new LatLng(sw.latitude - (zoomN - deltaLat / 2), sw.longitude);
+            ne = new LatLng(ne.latitude + (zoomN - deltaLat / 2), ne.longitude);
+            bounds = new LatLngBounds(sw, ne);
+        }
+        else if (deltaLon < zoomN) {
+            sw = new LatLng(sw.latitude, sw.longitude - (zoomN - deltaLon / 2));
+            ne = new LatLng(ne.latitude, ne.longitude + (zoomN - deltaLon / 2));
+            bounds = new LatLngBounds(sw, ne);
         }
 
+        return bounds;
     }
-
-
-    public boolean checkLocationPermission() {
-        String permission = "android.permission.ACCESS_FINE_LOCATION";
-        int res = this.checkCallingOrSelfPermission(permission);
-        return (res == PackageManager.PERMISSION_GRANTED);
-    }
-
 }
